e86655cda864d8834123029cc01f75e05715e538
Roxane Bacon <roxane.bacon@etu.univ-cotedazur.fr>
2025-05-07 12:07:42 +0200

    ajout style bouton roll dice

--- front-end/src/components/Dice.tsx
+++ front-end/src/components/Dice.tsx
@@ -6,21 +6,35 @@ type Props = {
 };
 
 const Dice: React.FC<Props> = ({ onRoll }) => {
-    const roll = () => onRoll(Math.floor(Math.random() * 6) + 1);
-    return (
-        <button
-            onClick={roll}
-            style={{
-                position: 'absolute',
-                bottom: 20,
-                right: 20,
-                padding: '1rem 2rem',
-                fontSize: '1.5rem',
-            }}
-        >
-            🎲
-        </button>
-    );
+const roll = () => onRoll(Math.floor(Math.random() * 6) + 1)
+
+return (
+    <button
+    onClick={roll}
+    style={{
+        position: 'absolute',
+        bottom: 20,
+        right: 20,
+        padding: '0.4rem',
+        borderRadius: '0.75rem',
+        border: '1px solid #ccc',
+        backgroundColor: '#fff',
+        boxShadow: '0 4px 8px rgba(0,0,0,0.1)',
+        cursor: 'pointer',
+    }}
+    >
+    <img
+        src={'assets/bouton_dice.png'}
+        alt="Roll Dice"
+        style={{
+        display: 'block',
+        width: '5rem',
+        height: '5rem',
+        borderRadius: '0.5rem',
+        }}
+    />
+    </button>
+)
 };
 
 export default Dice;
