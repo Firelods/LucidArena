cc4e2ffa44710f4a5dbf80886b921e047cf67acd
Roxane Bacon <roxane.bacon@etu.univ-cotedazur.fr>
2025-05-30 18:51:35 +0700

    retrait true attach control

--- front-end/src/scenes/Board.ts
+++ front-end/src/scenes/Board.ts
@@ -47,7 +47,7 @@ export async function initBoard(
     scene,
   );
   scene.activeCamera = camera;
-  camera.attachControl(scene.getEngine().getRenderingCanvas()!, true, true);
+  camera.attachControl(scene.getEngine().getRenderingCanvas()!, true);
 
   // 2) Lumière
   new HemisphericLight('light', new Vector3(0, 1, 0), scene).intensity = 0.8;
